e5b97e16cc13644a8a152518698b52e174b3aad7
magicCicero <cicerooantonio20@gmail.com>
2023-07-06 23:21:58 +0200

    excerices done

--- 01_04_07_23_Import_Export/01_import_export_lev1_1/data.js
+++ 01_04_07_23_Import_Export/01_import_export_lev1_1/data.js
@@ -1,2 +1,2 @@
-export const arrayNumbers = [12, 11, 1, 111, 144, 1355, 135];
+export const arrayNumbers = [12, 11, 1, 111, 144, 1355, 1352];
 export const arrayCars = ["bmw", "audi", "fiat", "ferrari", "opel"];
