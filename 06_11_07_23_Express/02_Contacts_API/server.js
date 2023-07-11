d8a6cd31f9ab47cb5d383efcfd526f3a3fc76aeb
magicCicero <cicerooantonio20@gmail.com>
2023-07-11 22:40:44 +0200

    excercises done and added comments

--- 06_11_07_23_Express/02_Contacts_API/server.js
+++ 06_11_07_23_Express/02_Contacts_API/server.js
@@ -11,22 +11,28 @@ app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
+// Erstelle leere Arrays
 let contacts = [];
 let formattedContacts = [];
 
+// Es werden die Kontakte geladen aus dem definierten Pfad. Die JSON Datei ist bereits beispielhaft gefüllt
 const readingContacts = () => {
   contacts = fs.readFileSync(contactURL, { encoding: "utf-8" });
+  //   Der Inhalt wird mit JSON Parse in ein JS OBjekt umgewandlet und der Variable zugewiesen bzw zwischengespeichert
   formattedContacts = JSON.parse(contacts);
+  //   Die Variable wird mit Map geprüft, wobei jedem Kontakt eine ID anhand des Indexes hinzugefügt wird.Mit dem SpreadOperator werden alle Felder beibehalten und werden nach dem ID Eintrag angehängt.
   contacts = formattedContacts.map((contact, index) => ({
     id: index + 1,
     ...contact,
   }));
 };
 
+// Die Kontakte werden hier immer überschrieben, wenn die Funktion aufgerufen wird
 const writingContacts = () => {
   fs.writeFileSync(contactURL, JSON.stringify(contacts, null, 2));
 };
 
+// Alle Kontakte werden geladen und die vorherigen Funktionen aufgerufen
 app.get("/contacts", (req, res) => {
   readingContacts();
   writingContacts();
@@ -34,9 +40,13 @@ app.get("/contacts", (req, res) => {
   res.send(contacts);
 });
 
+// Ein Kontakt wird anhand der ID aufgerufen. Die req.params.id wird dabei als Number umgewandelt.
+// Die Variable Contacts wird anhand der Find Funktion und der entsprechendne ID durchlaufen.
+// Die Response wird anhand einer Bedingung zurückgegeben.
 app.get("/contacts/:id", (req, res) => {
   const id = Number(req.params.id);
   const contact = contacts.find((contact) => contact.id === id);
+  //   const contact = contacts.filter((contact) => contact.id === contact.id);
   if (contact) {
     res.status(200).send(contact);
   } else {
@@ -44,6 +54,7 @@ app.get("/contacts/:id", (req, res) => {
   }
 });
 
+// User kann auf dem Pfad einen neuen Contact hinzufügen. Die Req.body wird zwischengespeichert und in dem bestehenden JSON gepusht. Anschließnd wird die lokale Datei überschrieben
 app.post("/contacts", (req, res) => {
   const newContact = req.body;
   contacts.push(newContact);
@@ -51,6 +62,7 @@ app.post("/contacts", (req, res) => {
   res.status(201).send(newContact);
 });
 
+// User kann anhand einer aufgerufenen Contact ID die entsprechenden Kontakt löschen. Dabei wird die filter Methode angewendet, der ebenfalls nach der ID sucht
 app.delete("/contacts/:id", (req, res) => {
   const id = Number(req.params.id);
   contacts = contacts.filter((contact) => contact.id !== id);
