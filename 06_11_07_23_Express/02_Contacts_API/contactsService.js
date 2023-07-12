98403c35e4f181cbfe6a3ac71e0621e5f1f27e41
magicCicero <cicerooantonio20@gmail.com>
2023-07-12 20:24:27 +0200

    destructure server.js to export import functions

---
+++ 06_11_07_23_Express/02_Contacts_API/contactsService.js
@@ -0,0 +1,45 @@
+import fs from "fs";
+import express from "express";
+const app = express();
+
+const contactURL = new URL("contacts.json", import.meta.url);
+// Erstelle leere Arrays
+
+let contacts = [];
+
+// Die Kontakte werden hier immer überschrieben, wenn die Funktion aufgerufen wird
+export const writingContacts = async (newContact) => {
+  let contacts = fs.readFileSync(contactURL, { encoding: "utf-8" });
+  contacts = JSON.parse(contacts);
+  let upToDateContacts = contacts.push(newContact);
+  upToDateContacts = fs.writeFileSync(
+    contactURL,
+    JSON.stringify(contacts, null, 2)
+  );
+  console.log(contacts);
+  return contacts;
+};
+
+// Es werden die Kontakte geladen aus dem definierten Pfad. Die JSON Datei ist bereits beispielhaft gefüllt
+export const readingContacts = async () => {
+  contacts = fs.readFileSync(contactURL, { encoding: "utf-8" });
+  //   Der Inhalt wird mit JSON Parse in ein JS OBjekt umgewandlet und der Variable zugewiesen bzw zwischengespeichert
+  let formattedContacts = JSON.parse(contacts);
+  //   Die Variable wird mit Map geprüft, wobei jedem Kontakt eine ID anhand des Indexes hinzugefügt wird.Mit dem SpreadOperator werden alle Felder beibehalten und werden nach dem ID Eintrag angehängt.
+  contacts = formattedContacts.map((contact, index) => ({
+    id: index + 1,
+    ...contact,
+  }));
+  return contacts;
+};
+
+export const deletingContacts = async (deleteContact) => {
+  await fs.promises.writeFile(
+    contactURL,
+    JSON.stringify(deleteContact, null, 2)
+  );
+  let contacts = JSON.parse(
+    await fs.promises.readFile(contactURL, { encoding: "utf-8" })
+  );
+  return contacts;
+};
--- 06_11_07_23_Express/02_Contacts_API/server.js
+++ 06_11_07_23_Express/02_Contacts_API/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import fs from "fs";
+import {
+  writingContacts,
+  readingContacts,
+  deletingContacts,
+} from "./contactsService.js";
 
 const app = express();
 const PORT = 9898;
-const contactURL = new URL("contacts.json", import.meta.url);
 
 app.use(express.json());
 
@@ -12,38 +16,20 @@ app.get("/", (req, res) => {
 });
 
 // Erstelle leere Arrays
+
 let contacts = [];
-let formattedContacts = [];
-
-// Es werden die Kontakte geladen aus dem definierten Pfad. Die JSON Datei ist bereits beispielhaft gefüllt
-const readingContacts = () => {
-  contacts = fs.readFileSync(contactURL, { encoding: "utf-8" });
-  //   Der Inhalt wird mit JSON Parse in ein JS OBjekt umgewandlet und der Variable zugewiesen bzw zwischengespeichert
-  formattedContacts = JSON.parse(contacts);
-  //   Die Variable wird mit Map geprüft, wobei jedem Kontakt eine ID anhand des Indexes hinzugefügt wird.Mit dem SpreadOperator werden alle Felder beibehalten und werden nach dem ID Eintrag angehängt.
-  contacts = formattedContacts.map((contact, index) => ({
-    id: index + 1,
-    ...contact,
-  }));
-};
-
-// Die Kontakte werden hier immer überschrieben, wenn die Funktion aufgerufen wird
-const writingContacts = () => {
-  fs.writeFileSync(contactURL, JSON.stringify(contacts, null, 2));
-};
-
 // Alle Kontakte werden geladen und die vorherigen Funktionen aufgerufen
 app.get("/contacts", (req, res) => {
-  readingContacts();
-  writingContacts();
-  console.log(contacts);
-  res.send(contacts);
+  readingContacts().then((contacts) => {
+    res.json(contacts);
+  });
 });
 
 // Ein Kontakt wird anhand der ID aufgerufen. Die req.params.id wird dabei als Number umgewandelt.
 // Die Variable Contacts wird anhand der Find Funktion und der entsprechendne ID durchlaufen.
 // Die Response wird anhand einer Bedingung zurückgegeben.
-app.get("/contacts/:id", (req, res) => {
+app.get("/contacts/:id", async (req, res) => {
+  let contacts = await readingContacts();
   const id = Number(req.params.id);
   const contact = contacts.find((contact) => contact.id === id);
   //   const contact = contacts.filter((contact) => contact.id === contact.id);
@@ -55,19 +41,19 @@ app.get("/contacts/:id", (req, res) => {
 });
 
 // User kann auf dem Pfad einen neuen Contact hinzufügen. Die Req.body wird zwischengespeichert und in dem bestehenden JSON gepusht. Anschließnd wird die lokale Datei überschrieben
-app.post("/contacts", (req, res) => {
+app.post("/contacts", async (req, res) => {
   const newContact = req.body;
-  contacts.push(newContact);
-  writingContacts();
-  res.status(201).send(newContact);
+  let newContactEntry = await writingContacts(newContact);
+  res.status(201).send(newContactEntry);
 });
 
 // User kann anhand einer aufgerufenen Contact ID die entsprechenden Kontakt löschen. Dabei wird die filter Methode angewendet, der ebenfalls nach der ID sucht
-app.delete("/contacts/:id", (req, res) => {
+app.delete("/contacts/:id", async (req, res) => {
   const id = Number(req.params.id);
-  contacts = contacts.filter((contact) => contact.id !== id);
-  writingContacts();
-  res.status(204).send("Kontakt gelöscht");
+  let contacts = await readingContacts();
+  let deleteContact = contacts.filter((contact) => contact.id !== id);
+  await deletingContacts(deleteContact);
+  res.status(200).send("Kontakt gelöscht");
 });
 
 app.listen(PORT, () => {
