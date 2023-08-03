fef8a2d42ef14a7759d243e7f5fd8e88d6f5bba8
magicCicero <cicerooantonio20@gmail.com>
2023-08-03 12:38:28 +0200

    added relational and embedded methods

---
+++ 17_28_07_23_DB_Relations/mongoose/relations/relationalDocuments.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+await mongoose.connect("mongodb://localhost:27017/relation-test");
+
+const inventorySchema = new mongoose.Schema({
+  name: String,
+  category: {
+    type: String,
+    enum: ["bigstuff", "smallstuff"],
+  },
+});
+
+const Inventory = mongoose.model("Inventory", inventorySchema);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  inventory: [{type: mongoose.Types.ObjectId,ref: "Inventory"}],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const invent = await Inventory.findOneAndUpdate(
+  { name: "Couch" },
+  { name: "Couch", category: "bigstuff" },
+  { upsert: true }
+);
+console.log(invent)
+//{upsert: true} update or insert wenn kein user mit dem Namen Bernd gefunden wird
+
+const user = await User.findOne({name: "Bernd"}).populate("inventory")
+// user.inventory.push(invent)
+// await user.save()
+console.log(user)
+
+await mongoose.disconnect();
